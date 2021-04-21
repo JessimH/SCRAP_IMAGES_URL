aa1e244d41e6367ef64ed47edbb8bcfcabfac25a
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-21 13:25:22 +0200

    first commit

---
+++ scrapImages.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+page = requests.get("https://www.lafermeducbd.fr/vaporisateurs/")
+souped = BeautifulSoup(page.content, "html.parser")
+imgs = souped.find_all("img")
+imgs = imgs[3:-1]
+
+for img in tqdm(imgs):
+    imgLink = img.attrs.get("src")
+    image = requests.get(imgLink).content
+    filename = r"scraping" + imgLink[imgLink.rfind("/"):]
+    with open(filename, "wb") as file:
+        file.write(image)
