1cdcd6554560dbdd68b1d4e17c0906694d0189c4
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-04-21 13:36:54 +0200

    comments

--- scrapImages.py
+++ scrapImages.py
@@ -1,15 +1,21 @@
+
+#requests allows us to play with Urls
 import requests
+
+#BeautifulSoup is a webscraping module 
 from bs4 import BeautifulSoup
+
+# Tqdm just show a progressbar in order to see when the loop ends.
 from tqdm import tqdm
 
 print('Enter an url with beautiful images:')
 url = input()
 page = requests.get(url)
 souped = BeautifulSoup(page.content, "html.parser")
-imgs = souped.find_all("img")
-imgs = imgs[3:-1]
+img_arr = souped.find_all("img")
+img_arr = img_arr[3:-1]
 
-for img in tqdm(imgs):
+for img in tqdm(img_arr):
     imgLink = img.attrs.get("src")
     image = requests.get(imgLink).content
     filename = r"scraping" + imgLink[imgLink.rfind("/"):]
